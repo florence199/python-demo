bfd87568a527afd3e2b42aa99268a911907c74ad
florence199 <97746278+florence199@users.noreply.github.com>
2022-01-14 10:22:31 -0500

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,20 @@
+#print("hello florence")
+#print(8+9)
+#print("What's the value of 8+9 = ", 8+9)
+
+
+a = 5
+print(a)
+print("value of a is: ", a)
+
+print("---------------------------------------")
+
+b = "hello"
+d = "  "
+c = "florence"
+print(b+d+c)
+
+print("---------------------------------------")
+e = 5.7
+f = 8.9
+print("value of e+f =", e+f)
